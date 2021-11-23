4d91ebef9f7d5a18fff1e89fab2c2f8f28f221e4
saltukkos <saltukkos@gmail.com>
2021-11-23 22:39:41 +0700

    Init the repository

---
+++ source/WiseMerge.CLI/Program.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WiseMerge.CLI;
+
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Out.WriteLine("It's the beginning of something cool!");
+    }
+}
